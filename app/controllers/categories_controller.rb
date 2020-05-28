1c890bfb8b37c61cdd93807c10cb3d4fe2fc86c6
chiharakenta <chiharakenta0519@gmail.com>
2020-05-28 10:15:31 +0900

    add category crud

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,37 @@
+class CategoriesController < ApplicationController
+  def index
+    @category = Category.new
+    @categories = Category.all
+  end
+
+  def create
+    @category = Category.new(category_params)
+    @category.save
+    redirect_to categories_path
+  end
+
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      redirect_to categories_path
+    else
+      render edit
+    end
+  end
+
+  def destroy
+    @category = Category.find(params[:id])
+    @category.destroy
+    redirect_to categories_path
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'todos#index'
   resources :todos, only: [:index, :new, :create, :edit, :update, :destroy]
+  resources :categories, only: [:index, :create, :edit, :update, :destroy]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
